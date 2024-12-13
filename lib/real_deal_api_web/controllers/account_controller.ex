6b05669c90ed1b72f8c2aaffc6e72eddb11b8251
Namit <namit.k.patel@gmail.com>
2024-12-13 18:55:16 +0530

    account session with plug.conn

--- lib/real_deal_api_web/controllers/account_controller.ex
+++ lib/real_deal_api_web/controllers/account_controller.ex
@@ -26,6 +26,7 @@ defmodule RealDealApiWeb.AccountController do
     case Guardian.authenticate(email, hash_password) do
       {:ok, account, token} ->
         conn
+        |> Plug.Conn.put_session(:account_id, account.id)
         |> put_status(:ok)
         |> render(:account_token, %{account: account, token: token})
 
@@ -35,8 +36,8 @@ defmodule RealDealApiWeb.AccountController do
   end
 
   def show(conn, %{"id" => id}) do
-    account = Accounts.get_account!(id)
-    render(conn, :show, account: account)
+    # account = Accounts.get_account!(id)
+    render(conn, :show, account: conn.assigns.account)
   end
 
   def update(conn, %{"id" => id, "account" => account_params}) do
