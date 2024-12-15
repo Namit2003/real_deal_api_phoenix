4af810eee2721e76fc15b6bcd4e850091a050faf
Namit <namit.k.patel@gmail.com>
2024-12-15 19:52:50 +0530

    changeset tests done

--- test/real_deal_api/schema/account_test.exs
+++ test/real_deal_api/schema/account_test.exs
@@ -1,5 +1,6 @@
 defmodule RealDealApi.Schema.AccountTest do
   use ExUnit.Case
+  alias Ecto.Changeset
   alias RealDealApi.Accounts.Account
 
   @expected_fields_with_types [
@@ -9,6 +10,7 @@ defmodule RealDealApi.Schema.AccountTest do
     {:inserted_at, :utc_datetime},
     {:updated_at, :utc_datetime}
   ]
+  @optional [:id, :inserted_at, :updated_at]
 
   describe "field and type" do
     test "it has the correct fields and types" do
@@ -19,6 +21,77 @@ defmodule RealDealApi.Schema.AccountTest do
         end
 
       assert MapSet.new(actual_fields_with_types) == MapSet.new(@expected_fields_with_types)
+    end
+  end
+
+  describe "changeset/2" do
+    test "success: returns a valid changeset when given valid arguments" do
+      valid_params = %{
+        "id" => Ecto.UUID.generate(),
+        "email" => "test@email.com",
+        "hash_password" => "test password",
+        "inserted_at" => DateTime.truncate(DateTime.utc_now(), :second),
+        "updated_at" => DateTime.truncate(DateTime.utc_now(), :second)
+      }
+
+      changeset = Account.changeset(%Account{}, valid_params)
+
+      assert %Changeset{valid?: true, changes: changes} = changeset
+
+      mutated = [:hash_password]
+
+      for {field, _} <- @expected_fields_with_types, field not in mutated do
+        actual = Map.get(changes, field)
+        expected = valid_params[Atom.to_string(field)]
+
+        assert actual == expected,
+               "Values did not match for field: #{field}\nexpected: #{inspect(expected)}\nactual: #{inspect(actual)}"
+      end
+
+      assert Pbkdf2.verify_pass(valid_params["hash_password"], changes.hash_password),
+             "Password: #{inspect(valid_params["hash_password"])} does not match \nhash: #{inspect(changes.hash_password)}"
+    end
+
+    test "error: returns an error changeset when given un-castable values" do
+      invalid_params = %{
+        "id" => DateTime.truncate(DateTime.utc_now(), :second),
+        "email" => DateTime.truncate(DateTime.utc_now(), :second),
+        "hash_password" => DateTime.truncate(DateTime.utc_now(), :second),
+        "inserted_at" => "lets put a string here",
+        "updated_at" => "lets put a string here"
+      }
+
+      assert %Changeset{valid?: false, errors: errors} =
+               Account.changeset(%Account{}, invalid_params)
+
+      for {field, _} <- @expected_fields_with_types do
+        assert errors[field], "The field: #{field} is missing from errors."
+
+        {_m, meta} = errors[field]
+
+        assert meta[:validation] == :cast,
+               "The validation type, #{meta[:validation]}, is incorrect."
+      end
+    end
+
+    test "error: returns an error changeset when required fields are missing" do
+      params = %{}
+
+      assert %Changeset{valid?: false, errors: errors} =
+               Account.changeset(%Account{}, params)
+
+      for {field, _} <- @expected_fields_with_types, field not in @optional do
+        assert errors[field], "The field: #{field} is missing from errors."
+
+        {_m, meta} = errors[field]
+
+        assert meta[:validation] == :required,
+               "The validation type, #{meta[:validation]}, is incorrect."
+      end
+
+      for field <- @optional do
+        refute errors[field], "The optional field #{field} is required when it shouldn't be."
+      end
     end
   end
 end
