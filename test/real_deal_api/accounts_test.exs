3b43a06e4400f6c60ed5f671c0244277c32f30f3
Namit <namit.k.patel@gmail.com>
2024-12-27 19:26:54 +0530

    update account test done

--- test/real_deal_api/accounts_test.exs
+++ test/real_deal_api/accounts_test.exs
@@ -38,4 +38,55 @@ defmodule RealDealApi.AccountsTest do
       assert {:error, %Changeset{valid?: false}} = Accounts.create_account(missing_params)
     end
   end
+
+  describe "get_account/1" do
+    test "success: it returns an account when given an valid UUID" do
+      existing_account = Factory.insert(:account)
+      assert returned_account = Accounts.get_account!(existing_account.id)
+      assert returned_account == existing_account
+    end
+
+    test "error: raises an Ecto.NoResultsError when an account doesn't exist" do
+      assert_raise Ecto.NoResultsError, fn ->
+        Accounts.get_account!(Ecto.UUID.autogenerate())
+      end
+    end
+  end
+
+  describe "update_account/2" do
+    test "success: it updates db and returns the account" do
+      existing_account = Factory.insert(:account)
+
+      params =
+        Factory.string_params_for(:account)
+        |> Map.take(["email"])
+
+      assert {:ok, returned_account} = Accounts.update_account(existing_account, params)
+
+      account_from_db = Repo.get(Account, returned_account.id)
+
+      assert returned_account == account_from_db
+
+      expected_account_data =
+        existing_account
+        |> Map.from_struct()
+        |> Map.put(:email, params["email"])
+
+      for {field, expected} <- expected_account_data do
+        actual = Map.get(account_from_db, field)
+
+        assert actual == expected,
+               "Values did not match for field: #{field}\nexpected: #{inspect(expected)}\nactual: #{inspect(actual)}"
+      end
+    end
+
+    test "error: returns an error tuple when account can't be updated" do
+      existing_account = Factory.insert(:account)
+      bad_params = %{"email" => NaiveDateTime.utc_now()}
+
+      assert {:error, %Changeset{}} = Accounts.update_account(existing_account, bad_params)
+
+      assert existing_account == Repo.get(Account, existing_account.id)
+    end
+  end
 end
